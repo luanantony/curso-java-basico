30661ea20a8a8db1b7319b66bcbb968e2cf1af8a
Luan Antony <luanantony29@gmail.com>
2020-08-13 20:42:14 -0300

    13.08.20

--- curso-java-basico/src/com/luan/cursojava/aula17/labs/Exer08.java
+++ curso-java-basico/src/com/luan/cursojava/aula17/labs/Exer08.java
@@ -1,9 +1,31 @@
 package com.luan.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer08 {
 
+	private static Scanner scan;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		scan = new Scanner(System.in);
+		
+		int num;
+		double media;
+		int soma = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("Digite um número: ");
+			num = scan.nextInt();
+			
+			soma += num;
+			
+		}
+		
+		media = soma / 5;
+		System.out.println("A soma é " + soma);
+		System.out.println("A média é " + media);
 
 	}
 
--- curso-java-basico/src/com/luan/cursojava/aula17/labs/Exer10.java
+++ curso-java-basico/src/com/luan/cursojava/aula17/labs/Exer10.java
@@ -1,9 +1,24 @@
 package com.luan.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer10 {
 
+	private static Scanner scan;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		scan = new Scanner(System.in);
+		
+		
+		System.out.println("Entre com o primeiro número: ");
+		int num1 = scan.nextInt();
+		
+		System.out.println("Entre com o segundo número: ");
+		int num2 = scan.nextInt();
+		
+		for (int i = num1; i <= num2; i++) {
+			System.out.println(i);
+		}
 
 	}
 
