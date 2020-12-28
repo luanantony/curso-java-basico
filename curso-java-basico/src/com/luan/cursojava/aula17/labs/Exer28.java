e58d17f9a9ac25d2953890a744176118c66c5d7e
Luan Antony <luanantony29@gmail.com>
2020-12-28 20:28:43 -0300

    28.12.2020

---
+++ curso-java-basico/src/com/luan/cursojava/aula17/labs/Exer28.java
@@ -0,0 +1,29 @@
+package com.luan.cursojava.aula17.labs;
+
+import java.util.Scanner;
+
+public class Exer28 {
+
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+
+		scan = new Scanner(System.in);
+
+		System.out.println("Digite o número: ");
+		int num = scan.nextInt();
+
+		boolean primo = true;
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				System.out.println("Não é primo - divisível por " + i);
+				primo = false;
+			}
+		}
+		if (primo) {
+			System.out.println("É número primo");
+		}
+
+	}
+}
---
+++ curso-java-basico/src/com/luan/cursojava/aula17/labs/Exer30.java
@@ -0,0 +1,48 @@
+package com.luan.cursojava.aula17.labs;
+
+import java.util.Scanner;
+
+public class Exer30 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int numInicio;
+		int numFinal;
+		int num;
+		int resultado;
+
+		boolean invalido = true;
+
+		do {
+
+			System.out.println("Montar a tabuada de: ");
+			num = scan.nextInt();
+
+			System.out.println("Começar por: ");
+			numInicio = scan.nextInt();
+
+			System.out.println("Terminar em: ");
+			numFinal = scan.nextInt();
+
+			if (numFinal > numInicio) {
+				invalido = false;
+			}
+
+		} while (invalido);
+
+		System.out.println("Vou montar a tabuada de: " + num + " começando em " + numInicio + " e terminando em "
+				+ numFinal + ": ");
+
+		for (int i = numInicio; i <= numFinal; i++) {
+
+			resultado = i * num;
+
+			System.out.println(num + " X " + i + " = " + resultado);
+
+		}
+		scan.close();
+	}
+
+}
